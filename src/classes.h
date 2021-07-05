db65c7ad078523d74d57ff971c28c84ea902434c
Pieter David <pieter.david@gmail.com>
2021-07-05 15:36:00 +0200

    Build dictionary if not used from scikit-build

---
+++ src/classes.h
@@ -0,0 +1,5 @@
+#if defined(PROJECT_NAME) && defined(CMSSW_GIT_HASH)
+#include "UserCode/CMSJMECalculators/interface/JMESystematicsCalculators.h"
+#else
+#include "JMESystematicsCalculators.h"
+#endif
