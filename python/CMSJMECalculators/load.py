137e633aa26baa176de70088122cabf8e1c80eb4
Pieter David <pieter.david@gmail.com>
2021-07-05 16:04:43 +0200

    extend 'load' for the non-CMSSW non-pip case

--- python/CMSJMECalculators/load.py
+++ python/CMSJMECalculators/load.py
@@ -6,20 +6,29 @@ def loadJMESystematicsCalculators():
         gbl.gROOT.ProcessLine('#define PROJECT_NAME "CMSSW"')
         gbl.gROOT.ProcessLine('#define CMSSW_GIT_HASH {}'.format(os.environ["CMSSW_GIT_HASH"]))
         gbl.gROOT.ProcessLine('#include "UserCode/CMSJMECalculators/interface/JMESystematicsCalculators.h"')
-        getattr(gbl, "JetVariationsCalculator::result_t")  # trigger dictionary generation
-    else:  # pip version
+    else:
         import pkg_resources
-        incDir = pkg_resources.resource_filename("CMSJMECalculators", "include")
-        libDir = pkg_resources.resource_filename("CMSJMECalculators", "lib")
-        libName = "libCMSJMECalculators"
-        import os.path
-        import ROOT as gbl
-        st = gbl.gSystem.Load(os.path.join(libDir, libName))
-        if st == -1:
-            raise RuntimeError("Library {0} could not be found".format(libName))
-        elif st == -2:
-            raise RuntimeError("Version match for library {0}".format(libName))
-        gbl.gInterpreter.AddIncludePath(incDir)
-        gbl.gROOT.ProcessLine('#include "JMESystematicsCalculators.h"')
-        getattr(gbl, "JetVariationsCalculator::result_t")  # trigger dictionary generation
-
+        try:  # pip version
+            dist_info = pkg_resources.get_distribution("CMSJMECalculators")
+            incDir = pkg_resources.resource_filename("CMSJMECalculators", "include")
+            libDir = pkg_resources.resource_filename("CMSJMECalculators", "lib")
+            libName = "libCMSJMECalculators"
+            import os.path
+            import ROOT as gbl
+            st = gbl.gSystem.Load(os.path.join(libDir, libName))
+            if st == -1:
+                raise RuntimeError("Library {0} could not be found".format(libName))
+            elif st == -2:
+                raise RuntimeError("Version match for library {0}".format(libName))
+            gbl.gInterpreter.AddIncludePath(incDir)
+            gbl.gROOT.ProcessLine('#include "JMESystematicsCalculators.h"')
+        except pkg_resources.DistributionNotFound:  # fallback: load directly
+            # TODO guess path from __file__ and add to dynamic path?
+            libName = "libCMSJMECalculators"
+            import ROOT as gbl
+            st = gbl.gSystem.Load(libName)
+            if st == -1:
+                raise RuntimeError("Library {0} could not be found".format(libName))
+            elif st == -2:
+                raise RuntimeError("Version match for library {0}".format(libName))
+    getattr(gbl, "JetVariationsCalculator::result_t")  # trigger dictionary generation
