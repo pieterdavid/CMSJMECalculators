e57d6d895b0aec2a26d18b4051130fa1bbe01913
Pieter David <pieter.david@cern.ch>
2021-10-12 17:38:05 +0200

    Allow for pip install inside CMSSW (as external)

--- python/CMSJMECalculators/load.py
+++ python/CMSJMECalculators/load.py
@@ -1,34 +1,32 @@
 def loadJMESystematicsCalculators():
     import os
-    if "CMSSW_BASE" in os.environ:  # CMSSW/scram version
-        import ROOT as gbl
-        gbl.gSystem.Load("libUserCodeCMSJMECalculators")
-        gbl.gROOT.ProcessLine('#define PROJECT_NAME "CMSSW"')
-        gbl.gROOT.ProcessLine('#define CMSSW_GIT_HASH {}'.format(os.environ["CMSSW_GIT_HASH"]))
-        gbl.gROOT.ProcessLine('#include "UserCode/CMSJMECalculators/interface/JMESystematicsCalculators.h"')
-    else:
-        import pkg_resources
-        try:  # pip version
-            dist_info = pkg_resources.get_distribution("CMSJMECalculators")
-            incDir = pkg_resources.resource_filename("CMSJMECalculators", "include")
-            libDir = pkg_resources.resource_filename("CMSJMECalculators", "lib")
-            libName = "libCMSJMECalculators"
-            import os.path
-            import ROOT as gbl
-            st = gbl.gSystem.Load(os.path.join(libDir, libName))
-            if st == -1:
-                raise RuntimeError("Library {0} could not be found".format(libName))
-            elif st == -2:
-                raise RuntimeError("Version match for library {0}".format(libName))
-            gbl.gInterpreter.AddIncludePath(incDir)
-            gbl.gROOT.ProcessLine('#include "JMESystematicsCalculators.h"')
-        except pkg_resources.DistributionNotFound:  # fallback: load directly
+    import pkg_resources
+    import ROOT as gbl
+    try:  # pip version
+        pkg_resources.get_distribution("CMSJMECalculators")
+        incDir = pkg_resources.resource_filename("CMSJMECalculators", "include")
+        libDir = pkg_resources.resource_filename("CMSJMECalculators", "lib")
+        libName = "libCMSJMECalculators"
+        import os.path
+        st = gbl.gSystem.Load(os.path.join(libDir, libName))
+        if st == -1:
+            raise RuntimeError("Library {0} could not be found".format(libName))
+        elif st == -2:
+            raise RuntimeError("Version match for library {0}".format(libName))
+        gbl.gInterpreter.AddIncludePath(incDir)
+        gbl.gROOT.ProcessLine('#include "JMESystematicsCalculators.h"')
+    except pkg_resources.DistributionNotFound as ex:
+        if "CMSSW_BASE" in os.environ:  # CMSSW/scram version
+            gbl.gSystem.Load("libUserCodeCMSJMECalculators")
+            gbl.gROOT.ProcessLine('#define PROJECT_NAME "CMSSW"')
+            gbl.gROOT.ProcessLine('#define CMSSW_GIT_HASH {}'.format(os.environ["CMSSW_GIT_HASH"]))
+            gbl.gROOT.ProcessLine('#include "UserCode/CMSJMECalculators/interface/JMESystematicsCalculators.h"')
+        else:  # fallback: load directly
             libName = "libCMSJMECalculatorsDict"
-            import ROOT as gbl
             gbl.gSystem.AddDynamicPath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
             st = gbl.gSystem.Load(libName)
             if st == -1:
                 raise RuntimeError("Library {0} could not be found".format(libName))
             elif st == -2:
                 raise RuntimeError("Version match for library {0}".format(libName))
-    getattr(gbl, "JetVariationsCalculator::result_t")  # trigger dictionary generation
+    getattr(gbl, "JetVariationsCalculator::result_t")  # trigger dictionary generation (if needed)
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import os, os.path
 from setuptools import find_packages
 from setuptools_scm import get_version
 from skbuild import setup
+
+cmake_args = [
+        f"-DCMSJMECALCULATOR_VERSION:STRING={get_version()}",
+        ]
+if os.getenv("CMSSW_BASE") and os.getenv("ROOTSYS"):
+    rootdir= os.path.join(os.getenv("ROOTSYS"), "cmake")
+    if os.path.isdir(rootdir):
+        cmake_args.append(f"-DROOT_DIR={rootdir}")
 
 setup(
     packages=find_packages(where="python"),
     package_dir={"": "python"},
     cmake_install_dir="python",
-    cmake_args=[f"-DCMSJMECALCULATOR_VERSION:STRING={get_version()}"],
+    cmake_args=cmake_args
 )
